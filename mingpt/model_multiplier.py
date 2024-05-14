38558b6230ea62b9cca0e2c30fa787e86d71a809
Felicity Liu <fl@FelicitydeMacBook-Air.local>
2024-05-14 21:11:52 +0800

    first commit

--- mingpt/model_multiplier.py
+++ mingpt/model_multiplier.py
@@ -163,11 +163,13 @@ class GPT(nn.Module):
     def _init_weights(self, module):
         # TODO:  you may choose different initialization
         if isinstance(module, nn.Linear):
-            torch.nn.init.xavier_uniform_(module.weight, gain=1.0)
+            # torch.nn.init.xavier_uniform_(module.weight, gain=1.0)
+            torch.nn.init.normal_(module.weight, mean=0, std=0.02)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
-            torch.nn.init.torch.nn.init.xavier_uniform_(module.weight, gain=1.0)
+            # torch.nn.init.torch.nn.init.xavier_uniform_(module.weight, gain=1.0)
+            torch.nn.init.torch.nn.init.normal_(module.weight, mean=0, std=0.02)
         elif isinstance(module, nn.LayerNorm):
             torch.nn.init.zeros_(module.bias)
             torch.nn.init.ones_(module.weight)
